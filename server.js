c0810e72c859199cc4a1377d1ef8a00fe3427bc2
RafiDev <yohann.desravines@epitech.eu>
2021-04-22 13:40:23 +0200

    [MOD] -> server to upload file on digitalOcean v3

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ app.set('view engine', 'jade');
 
 app.use(cors());
 app.use(logger('dev'));
+app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
