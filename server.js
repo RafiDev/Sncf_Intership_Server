0718d05a7042942d228cfdcd4c5f47c159eac021
RafiDev <yohann.desravines@epitech.eu>
2021-04-22 14:13:38 +0200

    [MOD] -> server to upload file on digitalOcean v4

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 var createError = require("http-errors");
+var bodyParser = require('body-parser')
+
 
 const app = express();
 
@@ -12,8 +14,8 @@ app.set('view engine', 'jade');
 
 app.use(cors());
 app.use(logger('dev'));
-app.use(express.urlencoded({ extended: false}));
-app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
