da6da42f21ed726fe5e0971c6b7c7a2e334a885e
RafiDev <yohann.desravines@epitech.eu>
2021-04-23 09:20:04 +0200

    [MOD] -> server to upload file on digitalOcean v7

--- server.js
+++ server.js
@@ -12,21 +12,19 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-//app.use(cors());
+app.use(cors());
 app.use(logger('dev'));
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-/*
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
 });
-*/
 
 require('./routes/index')(app);
 require('./routes/rexmat')(app);
